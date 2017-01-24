02a5b27c98567eafdfb3d1631e210557a47370df
iganchev87 <iganchev87@gmail.com>
2017-01-24 13:42:57 +0200

    correction of variable days = 365

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace TimeSinceBirthday
         {
             Console.Write("Years - ");
             byte years = byte.Parse(Console.ReadLine());
-            int days = years * 356;
+            int days = years * 365;
             int hours = days * 24;
             int minutes = hours * 60;
             Console.WriteLine("{0} years = {1} days = {2} hours = {3} minutes.", years,days,hours,minutes);
